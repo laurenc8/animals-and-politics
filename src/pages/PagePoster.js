d455406359d7d4675ebbf83e0214dbbb239e72df
laurenc8 <laurenc8822@gmail.com>
2021-05-10 21:49:10 -0400

    move files

--- src/pages/PagePoster.js
+++ src/pages/PagePoster.js
@@ -4,7 +4,7 @@ import poster from '../assets/poster.png';
 function PagePoster() {
   return (
     <div style={{paddingTop: '60px', paddingBottom: '30px'}}>
-      <img src={poster} width="50%"/>
+      <img src={poster} alt={'Poster'} width="50%"/>
     </div>
   );
 };
