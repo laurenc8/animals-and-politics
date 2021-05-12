efebed11c92f0739c61779bd5133295b505013f9
laurenc8 <laurenc8822@gmail.com>
2021-05-12 11:47:43 -0400

    order of navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,9 +8,9 @@ function NavigationBar() {
       <Navbar expand="lg">
         <Navbar.Brand>Animals and Politics</Navbar.Brand>
         <Nav>
-          <Link to="/video">04. Video</Link>
-          <Link to="/tiktoks">03. Tiktoks</Link>
-          <Link to="/poster">02. Poster</Link>
+          <Link to="/tiktoks">04. Tiktoks</Link>
+          <Link to="/poster">03. Poster</Link>
+          <Link to="/video">02. Video</Link>
           <Link to="/">01. Home</Link>
         </Nav>
       </Navbar>
