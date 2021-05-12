020fd5cf97d58436d96b70e03e8f1b0fb5c0ae1d
laurenc8 <laurenc8822@gmail.com>
2021-05-11 22:32:26 -0400

    embed YT vid

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,6 +8,7 @@ function NavigationBar() {
       <Navbar expand="lg">
         <Navbar.Brand>Animals and Politics</Navbar.Brand>
         <Nav>
+          <Link to="/video">04. Video</Link>
           <Link to="/tiktoks">03. Tiktoks</Link>
           <Link to="/poster">02. Poster</Link>
           <Link to="/">01. Home</Link>
---
+++ src/pages/PageVideo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import YoutubeEmbed from '../components/ConnorVid';
+
+function PageVideo() {
+  return (
+    <div style={{paddingTop: '60px', paddingBottom: '30px'}}>
+      <YoutubeEmbed embedId="y7AJnDRuemA"/>
+    </div>
+  );
+};
+
+export default PageVideo;
